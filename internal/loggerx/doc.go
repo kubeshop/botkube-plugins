495bb13e5538a89e0246201b3650f8f0d3a6c0a4
Mateusz Szostok <szostok.mateusz@gmail.com>
2024-01-31 01:02:06 -0800

    Add Teams integration tests (#1361)

--- internal/loggerx/doc.go
+++
@@ -1,3 +0,0 @@
-// Package loggerx provides helper functions and extensions responsible for logging.
-// The 'x' in the package name stand for 'extension'. It is added to reduce possible conflicts with other packages named 'logger'.
-package loggerx
